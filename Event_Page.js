2a880a731647d61362f37482537b642a37924098
habdelghany <61607643+habdelghany2023@users.noreply.github.com>
2021-09-05 00:26:08 -0400

    Update Event_Page.js

--- Event_Page.js
+++ Event_Page.js
@@ -1,1 +1,16 @@
-
+function myFnc(e){
+  e.classList.toggle("show");
+  let elem = document.getElementById("navID"),Style = window.getComputedStyle(elem),right=Style.getPropertyValue("right");
+  if(right == "0px"){
+    elem.style.right = "-260px";
+  }
+  else{
+    elem.style.right = "0px";
+  }
+  /*if(right == "0px"){
+    elem.style.right = "-260px";
+  }
+  else{
+    elem.style.right = "0px";
+  }*/
+}
